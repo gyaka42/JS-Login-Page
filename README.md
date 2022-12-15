0ff03ed515a7fba3b7c874b68821e362bb513d5d
gyaka42 <g_yaka@hotmail.com>
2022-12-16 00:37:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>JavaScript Login Page</h1>
+
+A small project of a login page.
+
+<h2>Techniques used for this project</h2>
+
+Filled it up with HTML, styled it with custom CSS properties with "var()" to use shortcuts repeated values. Also scripted it with JavaScript.
+
+<h2> Screen gif of final result</h2>
+
+![](screen.gif)
---
+++ main.js
@@ -0,0 +1,63 @@
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form_message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--succes", "form__message--error");
+    messageElement.classList.add("form__message--${type}");
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+} 
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#register");
+    const forgotPassword = document.querySelector("#forgot");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+        linkForgotPassword.classList.add("form--hidden")
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+        linkForgotPassword.classList.add("form--hidden")
+    });
+
+    document.querySelector("#linkForgotPassword").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+        linkForgotPassword.classList.add("form--hidden")
+    });
+
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        setFormMessage(loginForm, "HATA","Kullanici adi veya sifre yanlis");
+    });
+
+    document.querySelectorAll(".form_input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
